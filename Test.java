01f4db08ea5ddc8b47263d9907837562d7a132f8
Ayush Wunnava <66274690+tunna-7@users.noreply.github.com>
2023-01-04 18:11:40 +0530

    Java Part Done Almost🚀🚀🚀

--- Test.java
+++ Test.java
@@ -1,15 +1,21 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.nio.file.Files;
-import java.util.*;
+
+import org.json.JSONObject;
+
+
+
 
 public class Test {
+
+    final static String outputFilePath = "C:/Users/wayus/OneDrive/Desktop/Ayush Wunnava/Timepass/MyText.txt";
 
     public static void main(String[] args) throws IOException {
         String directory = "C:/Users/wayus/OneDrive/Desktop/Ayush Wunnava/Timepass/word cloud java/main";
@@ -34,10 +40,46 @@ public class Test {
                 mainMap.put(myKey[0], Arr);
             }
         }
-        System.out.println(mainMap.keySet().toArray()[5]);
-        System.out.println(mainMap.get(mainMap.keySet().toArray()[5]));
+        // System.out.println(mainMap.keySet().toArray()[5]);
+        // System.out.println(mainMap.get(mainMap.keySet().toArray()[5]));
 
         //Next step would be sending this map to javascript file
+
+        // Gson gson = new Gson();
+        // String jsonString = gson.toJson(mainMap);
+        // System.out.println(jsonString);
+        // String jsonString = new Gson().toJson(mainMap);
+        // JSONObject json = new JSONObject(mainMap);
+
+        // JSONObject resultMap = new JSONObject(mainMap);
+        // String result = resultMap.toString();
+        // System.out.println(result);
+
+        
+        //*****Writing to a file*****
+        File file = new File(outputFilePath);
+        BufferedWriter bf = null;
+        try {
+            bf = new BufferedWriter(new FileWriter(file));
+            for (Entry<String, ArrayList<String>> entry :
+                 mainMap.entrySet()) {
+                bf.write(entry.getKey() + ":"
+                         + entry.getValue());
+                bf.newLine();
+            }  
+            bf.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {  
+            try {  
+                bf.close();
+            }
+            catch (Exception e) {
+            }
+        }
+
     }
 
     public static List<String> getClasses(String directory) throws IOException {
